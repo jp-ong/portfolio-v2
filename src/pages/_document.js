6f65d88a0e97da5ef2c964f11f3eae08b6de8308
John Paul Ong <jpong5202@gmail.com>
2021-11-29 12:35:04 +0800

    Add font configuration

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&family=Quicksand&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
