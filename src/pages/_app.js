80672b3f15c9c0ae5ac17ce7f391fffa3843a48d
John Paul Ong <jpong5202@gmail.com>
2021-12-08 21:23:20 +0800

    Fix usage of next/script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,25 @@
 import "../styles/globals.scss";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-L173LGSTFH"
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-L173LGSTFH');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
