fae1bdb4e5f9b22ba69197215ddbf2b016b93e7c
John Paul Ong <jpong5202@gmail.com>
2021-12-08 21:01:06 +0800

    Fix usage of next/script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,30 @@
 import "../styles/globals.scss";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-L173LGSTFH"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-1KLVER5EP8');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
